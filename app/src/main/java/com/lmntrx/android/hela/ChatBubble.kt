5e271d4dd7bfe71bf07492a7731c550a86090ca5
livin21 <livinmathew99@gmail.com>
2018-03-24 12:11:42 +0530

    Made ChatBubble Serializable

--- app/src/main/java/com/lmntrx/android/hela/ChatBubble.kt
+++ app/src/main/java/com/lmntrx/android/hela/ChatBubble.kt
@@ -1,10 +1,12 @@
 package com.lmntrx.android.hela
 
+import java.io.Serializable
+
 
 /***
  * Created by Livin Mathew <mail@livinmathew.me> on 23/3/18.
  */
 
-sealed class ChatBubble(open val message: String)
-data class BotBubble(override val message: String): ChatBubble(message)
-data class UserBubble(override val message: String): ChatBubble(message)
+sealed class ChatBubble(open val message: String, open val type: Int): Serializable
+data class BotBubble(override val message: String): ChatBubble(message, 1)
+data class UserBubble(override val message: String): ChatBubble(message, 0)
