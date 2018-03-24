cb2b41401d424285e7aaeb89a6f1a3391d5adc8f
livin21 <livinmathew99@gmail.com>
2018-03-24 12:08:04 +0530

    Google login done

---
+++ app/src/main/java/com/lmntrx/android/hela/FirebaseHandler.kt
@@ -0,0 +1,12 @@
+package com.lmntrx.android.hela
+
+
+/***
+ * Created by Livin Mathew <mail@livinmathew.me> on 24/3/18.
+ */
+
+class FirebaseHandler{
+
+
+
+}
--- app/src/main/java/com/lmntrx/android/hela/MainActivity.kt
+++ app/src/main/java/com/lmntrx/android/hela/MainActivity.kt
@@ -1,31 +1,42 @@
 package com.lmntrx.android.hela
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var conversationList: ArrayList<ChatBubble>
-
+    private lateinit var conversationList: ArrayList<ChatBubble>
 
     var mode = 0
 
     val typingMode = 1
-    val idleMode = 0
+    private val idleMode = 0
+
+    private var account: GoogleSignInAccount? = null
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        conversationList = ArrayList()
-        conversationListRecyclerView.layoutManager = LinearLayoutManager(this)
-        conversationListRecyclerView.adapter = ConversationListAdapter(conversationList)
+        login()
 
+        // Setup RecyclerView
+        setupRecyclerView()
 
         // Click listener on send/mic button
         actionButton.setOnClickListener {
@@ -53,8 +64,12 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+    }
 
-
+    private fun setupRecyclerView() {
+        conversationList = ArrayList()
+        conversationListRecyclerView.layoutManager = LinearLayoutManager(this)
+        conversationListRecyclerView.adapter = ConversationListAdapter(conversationList)
     }
 
     private fun startMic() {
@@ -67,5 +82,49 @@ class MainActivity : AppCompatActivity() {
         conversationListRecyclerView.swapAdapter(ConversationListAdapter(conversationList), true)
         chatBox.setText("")
         mode = idleMode
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun login(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account == null) {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 0)
+        } else updateUser(account)
+    }
+
+
+    @SuppressLint("RestrictedApi")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 0) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            updateUser(account)
+        } catch (e: ApiException) {
+            Log.e("Login Error","Login Failed\n${e.message}")
+        }
+
+
+    }
+
+    private fun updateUser(account: GoogleSignInAccount?) {
+
+        this.account = account
+
     }
 }
